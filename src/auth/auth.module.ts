32778adb5781a47ea124e2f9312fc516ad33be18
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-01-17 14:07:12 +0200

    Generating JWT Access & Refresh Tokens into Login endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
 import { PrismaSerivce } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService, PrismaSerivce],
+  providers: [AuthService, UserService, PrismaSerivce, JwtService],
 })
 export class AuthModule {}
